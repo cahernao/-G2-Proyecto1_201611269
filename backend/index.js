15c84373f08c181b7413bc69874e4e27464eecd3
cahernao <hernandez.carlos09@gmail.com>
2025-04-18 21:34:46 -0600

    creating backend

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+// const multer = require('multer');
+// const predictNumber = require('./routes/predictNumber');
+// const predictFace = require('./routes/predictFace');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+// app.use('/uploads', express.static('uploads'));
+
+// app.use('/predict-number', predictNumber);
+// app.use('/predict-face', predictFace);
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
